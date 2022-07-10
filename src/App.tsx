e5720a46ff8182aae473da61f9e3a2a8bdfda1dc
김동구 <bbonggu92@gmail.com>
2022-07-10 18:18:10 +0900

    refactor: intersection 테스트용 SpaceDiv 정리

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <SpaceDiv />
       <AwardSection />
-      <SpaceDiv />
     </div>
   );
 }
 
 export default App;
-
-function SpaceDiv() {
-  return (
-    <div
-      style={{
-        height: 800,
-      }}
-    />
-  );
-}
